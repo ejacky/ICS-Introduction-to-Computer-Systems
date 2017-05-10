f3ffa3d5f463838017ec0c923788f19f83fa66c6
jackyzhang <852566802@qq.com>
2017-05-10 18:31:53 +0800

    modify tsh.c

--- labs/shlab/tsh.c
+++ labs/shlab/tsh.c
@@ -165,7 +165,33 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
-    return;
+	char *argv[MAXARGS];
+	char buf[MAXLINE];
+	int bg;
+	pid_t pid;
+	
+	strcpy(buf, cmdline);
+	bg = parseline(buf, argv);
+	if (argv[0] == NULL)
+		return;
+		
+	if (!builtin_command(argv)) {
+		if ((pid = Fork()) == 0) {
+			if (execve(argv[0], argv, environ) < 0) {
+				printf("%s: Command not found. \n", argv[0]);
+				exit(0);
+			}
+		}
+		
+		if (!bg) {
+			int status;
+			if (waitpid(pid, &status, 0) < 0) 
+				unix_error("waitfg: waitpid error");
+				exit(0);
+		} else
+			printf("%d %s", pid, cmdline);
+	}
+	return;
 }
 
 /* 
@@ -231,6 +257,10 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
+	if (!strcmp(argv[0], "quit"))
+		exit(0);
+	if (!strcmp(argv[0], "&"))
+		return 1;
     return 0;     /* not a builtin command */
 }
 
