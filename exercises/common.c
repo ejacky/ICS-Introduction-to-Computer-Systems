62f03cbb1b2d910c0ee569d307108b62d365057d
jackyzhang <852566802@qq.com>
2017-05-11 10:17:11 +0800

    modify common and add exersice 12

--- exercises/common.c
+++ exercises/common.c
@@ -11,6 +11,7 @@ void app_error(char *msg)
 	fprintf(stderr, "%s\n", msg);
 }
 
+// process
 pid_t Fork(void)
 {
 	pid_t pid;
@@ -20,14 +21,28 @@ pid_t Fork(void)
 	return pid;	
 }
 
-char *Fgets(char *ptr, int n, FILE *stream)
+void Execve(const char *filename, char *const argv[], char *const envp[])
 {
-	char *rptr;
+	if (execve (filename, argv, envp) < 0)
+		unix_error("Execve error");
+}
+
+pid_t Wait(int *status)
+{
+	pid_t pid;
 	
-	if (((rptr = fgets(ptr, n, stream)) == NULL) && ferror(stream))
-		app_error("Fgets error");
-		
-	return rptr;
+	if ((pid = wait(status)) < 0)
+		unix_error("Wait error");
+	return pid;
+}
+
+int Dup2(int fd1, int fd2)
+{
+	int rc;
+	
+	if ((rc = dup2(fd1, fd2)) < 0)
+		unix_error("Dup2 error");
+	return rc;
 }
 
 void Pause()
@@ -45,6 +60,7 @@ unsigned int Sleep(unsigned int secs)
 	return rc;
 }
 
+// signal
 void Kill(pid_t pid, int signum)
 {
 	int rc;
@@ -70,3 +86,115 @@ unsigned int Alarm(unsigned int seconds)
 {
 	return alarm(seconds);
 }
+
+// buffer
+void rio_readinitb(rio_t *rp, int fd)
+{
+	rp->rio_fd = fd;
+	rp->rio_cnt = 0;
+	rp->rio_bufptr = rp->rio_buf;
+}
+
+static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
+{
+	int cnt;	
+	
+	while (rp->rio_cnt <= 0) {
+		rp->rio_cnt = read(rp->rio_fd, rp->rio_buf, sizeof(rp->rio_buf));
+		if (rp->rio_cnt < 0) {
+			if  (errno != EINTR) return -1;
+		} else if (rp->rio_cnt == 0) return 0;
+		else rp->rio_bufptr = rp->rio_buf;
+	}
+	
+	cnt = n;
+	if (rp->rio_cnt < n) cnt = rp->rio_cnt;
+	memcpy(usrbuf, rp->rio_bufptr, cnt);
+	rp->rio_bufptr += cnt;
+	rp->rio_cnt -= cnt;
+	return cnt; 
+}
+
+ssize_t rio_written(int fd, void *usrbuf, size_t n)
+{
+	size_t nleft = n;
+	ssize_t nwritten;
+	char *bufp = usrbuf;
+	
+	while (nleft > 0) {
+		if ((nwritten = write(fd, bufp, nleft)) <=0) {
+			if (errno == EINTR) nwritten = 0;
+			else return -1; 
+		}
+		nleft -= nwritten;
+		bufp += nwritten;
+	}
+	return n;
+}
+
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
+{
+	int n, rc;
+	char c, *bufp = usrbuf;
+	
+	for (n = 1; n < maxlen; n++) {
+		if (rc = (rio_read(rp, &c, 1)) == 1) {
+			*bufp++ = c;
+			if (c == '\n') break;
+		} else if (rc == 0) {
+			if (n == 1) return 0;
+			else break;
+		} else return -1;
+	}
+} 
+
+void Rio_written(int fd, void *usrbuf, size_t n)
+{
+	if (rio_written(fd, usrbuf, n) != n) unix_error("Rio_written error");
+}
+
+// memory
+void *Mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
+{
+	void *ptr;
+	
+	if ((ptr = mmap(addr, len, prot, flags, fd, offset)) == ((void *) - 1 ))
+		unix_error("mmap error");
+	return (ptr);
+}
+
+void Munmap(void *start, size_t length)
+{
+	if (munmap(start, length) < 0)
+		unix_error("munmap error");
+}
+
+
+// file
+char *Fgets(char *ptr, int n, FILE *stream)
+{
+	char *rptr;
+	
+	if (((rptr = fgets(ptr, n, stream)) == NULL) && ferror(stream))
+		app_error("Fgets error");
+		
+	return rptr;
+}
+
+int Open(const char *pathname, int flags, mode_t mode)
+{
+	int rc;
+	
+	if ((rc = open(pathname, flags, mode)) < 0)
+		unix_error("Open error");
+	return rc;
+}
+
+void Close(int fd)
+{
+	int rc;
+	
+	if ((rc = close(fd)) < 0)
+		unix_error("close error");
+}
+
--- exercises/common.h
+++ exercises/common.h
@@ -1,13 +1,30 @@
-#include <signal.h> 
 #include <stdio.h>
+#include <sys/socket.h>
+#include <pthread.h>
 #include <sys/types.h>
-#include <stdlib.h>
-#include <string.h>
 #include <errno.h>
+#include <netinet/in.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 #include <setjmp.h>
+#include <signal.h>
 
+/* Í·ÎÄ¼þ */
 #define MAXLINE 8192
-#define MAXBUF  8192
+#define MAXBUF 8192
+#define RIO_BUFSIZE 8192
+
 extern char **environ;
 typedef void handler_t(int);
+
+typedef struct {
+	int rio_fd;
+	int rio_cnt;
+	char *rio_bufptr;
+	char rio_buf[RIO_BUFSIZE];
+} rio_t;
