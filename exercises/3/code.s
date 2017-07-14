2b8f85ec2f12a3565907a9309d2d1c76ddb020b2
jackyzhang <852566802@qq.com>
2017-07-14 18:35:44 +0800

    add 3

---
+++ exercises/3/code.s
@@ -0,0 +1,31 @@
+	.file	"code.c"
+	.text
+	.globl	sum
+	.type	sum, @function
+sum:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	12(%ebp), %eax
+	addl	8(%ebp), %eax
+	addl	%eax, accum
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	sum, .-sum
+	.globl	accum
+	.bss
+	.align 4
+	.type	accum, @object
+	.size	accum, 4
+accum:
+	.zero	4
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.3) 4.8.4"
+	.section	.note.GNU-stack,"",@progbits
